6dc328810e6a0af96b8c78ce051210354b756e83
9Prestidigitator <9prestidigitator@gmail.com>
2026-04-09 16:36:11 -0400

    Added overlay option

--- README.md
+++ README.md
@@ -17,3 +17,54 @@ You must add this line to your solution/projects `NuGet.Config` file:
 ```
 
 The `.nupkg` files will then be available via the environment variable `%NIX_NUGET%`.
+
+**Note:**
+When using [build-nuget-module](https://github.com/NixOS/nixpkgs/blob/96e87bd250d5f4f3447b87ab7e94689ea19e0c2a/pkgs/build-support/dotnet/build-dotnet-module/default.nix#L4) to derive a nuget packages you must have `packNupkg=true`. This ensures the .nupkg file is properly unloaded in the nix store.
+
+## Usage
+
+Simply add this library as a flake input:
+
+```nix
+inputs = {
+  nix-nuget-feed.url = "github:9Prestidigitator/nix-nuget-feed";
+}
+```
+
+You can now create a nix-nuget-feed development shell via the single library or an overlay.
+
+### Library
+
+```nix
+devshells.default = inputs.nix-nuget-feed.lib {
+  nugetPackages = [
+    inputs.myDotnetLib1.packages.${system}.default
+    inputs.myDotnetLib2.packages.${system}.default
+  ];
+  # Other development shell options...
+}
+```
+
+### Overlay
+
+```nix
+let
+  pkgs = import inputs.nixpkgs {
+    inherit system;
+    overlays = [ inputs.nix-nuget-feed.overlays.default ];
+  }
+in 
+{
+  devshells.default = pkgs.mkShell {
+    nugetPackages = [
+      inputs.myDotnetLib1.packages.${system}.default
+      inputs.myDotnetLib2.packages.${system}.default
+    ];
+    # Other development shell options...
+  }
+}
+```
+
+## Inspirations
+
+- [nuget-packageslock2nix](https://github.com/mdarocha/nuget-packageslock2nix): A great way to get your nuget.org packages into the nix store.
--- flake.nix
+++ flake.nix
@@ -1,37 +1,49 @@
 {
   description = "Nuget feed devshell helper";
 
-  outputs = {
-    lib.mkDotnetShell = {
+  outputs = {...}: let
+    mkNugetFeed = pkgs: nugetPackages:
+      pkgs.runCommand "nix-nuget-feed" {
+        buildInputs = [pkgs.zip pkgs.libxml2];
+      } ''
+        mkdir -p $out
+        ${
+          pkgs.lib.concatMapStringsSep "\n"
+          (pkg: ''
+            for versionDir in ${pkg}/share/nuget/packages/*/*; do
+              version=$(basename "$versionDir")
+              nuspec=$(find "$versionDir" -name "*.nuspec" | head -1)
+              pname=$(xmllint --xpath "string(//*[local-name()='id'])" "$nuspec")
+              (cd "$versionDir" && zip -r "$out/$pname.$version.nupkg" . && chmod 775 $out/*)
+            done
+          '')
+          nugetPackages
+        }
+      '';
+  in {
+    overlays.default = final: prev: {
+      mkShell = {nugetPackages ? [], ...} @ args: let
+        shellArgs = removeAttrs args ["nugetPackages"];
+      in
+        prev.mkShell (shellArgs
+          // {
+            env =
+              (args.env or {})
+              // prev.lib.optionalAttrs
+              (nugetPackages != [])
+              {NIX_NUGET = mkNugetFeed prev nugetPackages;};
+          });
+    };
+    lib = {
       pkgs,
-      localNugetPackages ? [],
+      nugetPackages ? [],
       ...
     } @ args: let
-      localNugetFeed =
-        pkgs.runCommand "local-nuget-feed" {
-          buildInputs = [pkgs.zip pkgs.libxml2];
-        } ''
-          mkdir -p $out
-          ${
-            pkgs.lib.concatMapStringsSep "\n"
-            (pkg: ''
-              for versionDir in ${pkg}/share/nuget/packages/*/*; do
-                version=$(basename "$versionDir")
-                nuspec=$(find "$versionDir" -name "*.nuspec" | head -1)
-                pname=$(xmllint --xpath "string(//*[local-name()='id'])" "$nuspec")
-                (cd "$versionDir" && zip -r "$out/$pname.$version.nupkg" . && chmod 775 $out/*)
-              done
-            '')
-            localNugetPackages
-          }
-        '';
-
-      # Strip our custom args before forwarding to mkShell
-      shellArgs = removeAttrs args ["pkgs" "localNugetPackages"];
+      shellArgs = removeAttrs args ["pkgs" "nugetPackages"];
     in
       pkgs.mkShell (shellArgs
         // {
-          env = (args.env or {}) // {NIX_NUGET = localNugetFeed;};
+          env = (args.env or {}) // pkgs.lib.optionalAttrs (nugetPackages != []) {NIX_NUGET = mkNugetFeed pkgs nugetPackages;};
         });
   };
 }
